b920d17d34b5038edd61e5628427585f7fee5645
Misha Golovach <golovach.miha01@gmail.com>
2023-06-08 17:52:07 +0300

    test

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => env('FILESYSTEM_ROOT', storage_path('app')),
             'throw' => false,
         ],
 
