619d3d1949b877b776c4827778178cb802f1383a
Misha Golovach <golovach.miha01@gmail.com>
2023-06-14 17:32:03 +0300

    yandex meetrika

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,13 +45,13 @@
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
             (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(93189013, "init", {
+            ym(93968327, "init", {
                 clickmap:true,
                 trackLinks:true,
                 accurateTrackBounce:true
             });
         </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/93189013" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <noscript><div><img src="https://mc.yandex.ru/watch/93968327" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <!-- /Yandex.Metrika counter -->
 
     </body>
