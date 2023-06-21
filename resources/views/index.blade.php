7110636cbffe176c6961a2799548f86d1260cde2
Misha Golovach <golovach.miha01@gmail.com>
2023-06-21 11:10:40 +0300

    some design fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Главная | MOON LOUNGE</title>
+        <title>Главная | MOON LOUNGE Люблино</title>
 
 
 
